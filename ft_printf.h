8fe464782bceab5b806e4bcaf57bc1d39447e0d8
Polpi <anthotuigr@gmail.com>
2022-11-06 11:53:30 +0100

    start_printf

---
+++ ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: polpi <polpi@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/06 11:46:01 by polpi             #+#    #+#             */
+/*   Updated: 2022/11/06 11:48:46 by polpi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
